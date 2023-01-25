98e69cbf6f9665615d5aecb8505a8283547c3e41
MindaugasPaskevicius <mindaugas.paske@gmail.com>
2023-01-25 14:18:11 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
