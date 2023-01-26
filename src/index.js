82029612fcf30ce1280dc8360bc9d36e638f8402
MindaugasPaskevicius <mindaugas.paske@gmail.com>
2023-01-26 19:45:33 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
-import React from "react";
+// import ReactDOM from "react-dom";
+// import { HashRouter } from "react-router-dom";
+// import App from "./App";
+// import "./index.css";
+// import React from "react";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+//import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+    <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+//reportWebVitals();
